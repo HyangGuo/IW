98df8d6a9cc3224e8803c7175c6b480ba8e2db8d
HyangGuo <87193566+HyangGuo@users.noreply.github.com>
2023-01-23 16:33:06 +0100

    refactoring

--- lab12-2022-home-original/src/test/java/it/unimi/di/sweng/lab12/presenter/PresenterInputTest.java
+++ lab12-2022-home-original/src/test/java/it/unimi/di/sweng/lab12/presenter/PresenterInputTest.java
@@ -5,8 +5,9 @@ import it.unimi.di.sweng.lab12.model.GroceryListModel;
 import it.unimi.di.sweng.lab12.view.InputView;
 
 import org.junit.jupiter.api.Test;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
+
 
 class PresenterInputTest {
     @Test
@@ -17,6 +18,23 @@ class PresenterInputTest {
         presenterInput.action(5,"pere1");
 
         verify(model,times(1)).addDaComprare(any(Grocery.class));
+    }
+    @Test
+    void  maxTest(){
+        InputView inputView = mock(InputView.class);
+        GroceryListModel model = new GroceryListModel();
+        PresenterInput presenterInput = new PresenterInput(model,inputView,8);
+        presenterInput.action(5,"pere1");
+        presenterInput.action(5,"pere2");
+        presenterInput.action(5,"pere3");
+        presenterInput.action(5,"pere4");
+        presenterInput.action(5,"pere5");
+        presenterInput.action(5,"pere6");
+        presenterInput.action(5,"pere7");
+        presenterInput.action(5,"pere8");
+        presenterInput.action(5,"pere9");
+
+        assertThat(model.getListDaComprare().size()).isEqualTo(8);
     }
 
 }
