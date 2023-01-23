35fcd2238418dc5bcd94de22167d8cb0ec7d77a6
HyangGuo <87193566+HyangGuo@users.noreply.github.com>
2023-01-23 16:05:25 +0100

    refectroing

--- lab12-2022-home-original/src/test/java/it/unimi/di/sweng/lab12/presenter/CommandPresenterTest.java
+++ lab12-2022-home-original/src/test/java/it/unimi/di/sweng/lab12/presenter/CommandPresenterTest.java
@@ -3,27 +3,48 @@ package it.unimi.di.sweng.lab12.presenter;
 import it.unimi.di.sweng.lab12.model.Grocery;
 import it.unimi.di.sweng.lab12.model.GroceryListModel;
 import it.unimi.di.sweng.lab12.view.CommandView;
-import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
 class CommandPresenterTest {
+    CommandView commandView;
+    CommandView commandView2;
+    GroceryListModel model;
+    CommandPresenter commandPresenter;
+
+    @BeforeEach
+    void init(){
+        commandView = mock(CommandView.class);
+        commandView2 = mock(CommandView.class);
+        model = mock(GroceryListModel.class);
+        commandPresenter = new CommandPresenter(model,commandView,commandView2,8);
+
+    }
     @Test
-    void updateModelTest(){
-        CommandView commandView = mock(CommandView.class);
-        CommandView commandView2 = mock(CommandView.class);
-        GroceryListModel model = new GroceryListModel();
-        model.addDaComprare(new Grocery("pere",2));
-        CommandPresenter commandPresenter = new CommandPresenter(model,commandView,commandView2,8);
+    void actionTest(){
+
         when(commandView.get(0)).thenReturn("pere");
         when(commandView2.get(0)).thenReturn("pere");
         commandPresenter.action(0,"pere");
-        assertThat(model.getListComprate().get(0).getNumber()).isEqualTo(1);
-    }
 
+        verify(model).reduceDaComprare(0);
+    }
+    @Test
+    void updateTest(){
+        List<Grocery> list = new ArrayList<>();
+        list.add(new Grocery("pere",2));
+        when(model.getListDaComprare()).thenReturn(list);
+        commandPresenter.update();
+        //initialization format "---" and update data , 16 times.
+        verify(commandView,times(16)).set(anyInt(),anyString());
+    }
 
 
 }
