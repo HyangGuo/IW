2573e7c3e30add937199a70768225cc9c76e80c7
HyangGuo <87193566+HyangGuo@users.noreply.github.com>
2023-01-23 16:16:04 +0100

    refactoring

--- lab12-2022-home-original/src/test/java/it/unimi/di/sweng/lab12/presenter/PresenterInputTest.java
+++ lab12-2022-home-original/src/test/java/it/unimi/di/sweng/lab12/presenter/PresenterInputTest.java
@@ -3,19 +3,20 @@ package it.unimi.di.sweng.lab12.presenter;
 import it.unimi.di.sweng.lab12.model.Grocery;
 import it.unimi.di.sweng.lab12.model.GroceryListModel;
 import it.unimi.di.sweng.lab12.view.InputView;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class PresenterInputTest {
     @Test
-    void presentInputTest(){
+    void actionTest(){
         InputView inputView = mock(InputView.class);
-        GroceryListModel model = new GroceryListModel();
-        model.addDaComprare(new Grocery("pere",2));
+        GroceryListModel model = mock(GroceryListModel.class);
         PresenterInput presenterInput = new PresenterInput(model,inputView,8);
-        presenterInput.action(5,"pere");
-        assertThat(model.getListDaComprare().get(0).getNumber()).isEqualTo(7);
+        presenterInput.action(5,"pere1");
+
+        verify(model,times(1)).addDaComprare(any(Grocery.class));
     }
+
 }
